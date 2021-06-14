d97b4f3774a8d0f824074f6f0133045612c92bb0
kammann123 <lucaskammann@hotmail.com>
2021-06-14 01:18:27 -0300

    Compiler generating .mif to intialize ROM memory Added ANK instruction Improvements to the compiler

--- ev21g1/hdl/generated/rom.v
+++ ev21g1/hdl/generated/rom.v
@@ -84,7 +84,7 @@ module rom (
 		altsyncram_component.address_aclr_a = "NONE",
 		altsyncram_component.clock_enable_input_a = "BYPASS",
 		altsyncram_component.clock_enable_output_a = "BYPASS",
-		altsyncram_component.init_file = "./../samples/tabla_2.mif",
+		altsyncram_component.init_file = "./../tools/object.mif",
 		altsyncram_component.intended_device_family = "Cyclone IV E",
 		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
 		altsyncram_component.lpm_type = "altsyncram",
